fe905a058559c084d1ec56912294f0e236f7552f
한재현 <restrain22@naver.com>
2022-05-19 18:33:15 +0900

    롬복 테스트

---
+++ src/test/java/com/restrain22/book/springboot/dto/HelloResponseDtoTest.java
@@ -0,0 +1,24 @@
+package com.restrain22.book.springboot.dto;
+
+
+import com.restrain22.book.springboot.web.dto.HelloResponseDto;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.Test;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void 롬복_기능_테스트(){
+        //given
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name,amount);
+
+        //assertj라는 테스트 검증 라이브러리의 검증 메소드, 검증받고 싶은 대상을 인자로 받음
+        //Junit의 assertThat이 아니라 assertj의 assertThat을 쓴 이유는 추가적인 라이브러리가 필요없고, 자동완성이 더 지원 잘 됨
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
